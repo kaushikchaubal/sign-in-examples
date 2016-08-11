cf8fe933886689ddb287df5a32bc489654806970
Kaushik Chaubal <kaushikchaubal@gmail.com>
2016-08-11 22:04:34 +0100

    Adding facebook sign-in

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A simple wep-page which shows how to log use different sign-in systems.
 
 ##Implemented so far:
 1. Google
+2. Facebook
 
 ## Demo?
 https://sign-in-examples.herokuapp.com
--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+// *******************************************
+// ********* GOOGLE SIGN-IN METHODS **********
+// *******************************************
 function onSignIn(googleUser) {
 	var profile = googleUser.getBasicProfile();
 	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -12,5 +15,47 @@ function signOut() {
 	auth2.signOut().then(function () {
 		console.log('User signed out.');
 		document.getElementById('google-status').innerHTML = 'Logged out successfully';
+	});
+}
+
+// *******************************************
+// ******* FACEBOOK SIGN-IN METHODS **********
+// *******************************************
+function statusChangeCallback(response) {
+	console.log('statusChangeCallback');
+	console.log(response);
+
+	if (response.status === 'connected') {
+	  signIn();
+	} else if (response.status === 'not_authorized') {
+	  document.getElementById('facebook-status').innerHTML = 'Please log into this app';
+	} else {
+	  document.getElementById('facebook-status').innerHTML = 'Please log into Facebook';
+	}
+}
+
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+}
+
+window.onload = function() {
+	FB.init({
+		appId      : '286260761740359',
+		xfbml      : true,
+		version    : 'v2.7'
+	});
+
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+};
+
+function signIn() {
+	console.log('Welcome!  Fetching your information.... ');
+	FB.api('/me', function(response) {
+		console.log('Successful login for: ' + response.name);
+		document.getElementById('facebook-status').innerHTML = "Welcome " + response.name;
 	});
 }
