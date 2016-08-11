c89033fcd67a668d233f5166ca75a10efea7b5af
Kaushik Chaubal <kaushikchaubal@gmail.com>
2016-08-11 21:12:28 +0100

    First commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
+	document.getElementById('google-status').innerHTML = "Welcome " + profile.getName();
+}
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+		document.getElementById('google-status').innerHTML = 'Logged out successfully';
+	});
+}
